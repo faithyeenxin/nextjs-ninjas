e5486e37854ce87a152a46b5472c61e8e68509a6
Faith Ye <faith.ye94@gmail.com>
2023-02-03 15:01:23 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  console.log("testing git account push again")
   return (<Layout>
   <Component {...pageProps} />
   </Layout>)
