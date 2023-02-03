6ce4772e0c5729137267b20b601efcf23887f1c7
Faith Ye <faith.ye.parallelchain.io>
2023-02-03 14:58:24 +0800

    testing ssh, commit should be by parallelchain account

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  console.log("testing git account push")
+  console.log("testing git account push again")
   return (<Layout>
   <Component {...pageProps} />
   </Layout>)
