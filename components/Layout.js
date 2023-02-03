f652282b54e1f6b5fd0f422a75822db68be1952b
faith.ye94 <faith.ye@parallelchain.io>
2023-02-03 14:22:10 +0800

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+
+const Layout = ({children}) => {
+
+    return (
+     <div className="content">
+        <NavBar />
+        {children}
+        <Footer />
+     </div>
+    );
+  };
+  
+export default Layout
+
---
+++ pages/ninjas/mini-ninja.js
@@ -0,0 +1,9 @@
+const MiniNinjaPage = () => {
+    console.log("hello world")
+
+    return (
+     <div>hello this is mini ninja</div>
+    );
+  };
+  
+  export default MiniNinjaPage
